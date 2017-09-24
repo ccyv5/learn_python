cacec769bdc7e3806d6e856c87a849ef86e92b70
ccyv5 <991522275@qq.com>
2017-09-24 22:30:57 +0800

    开始学习python

---
+++ learn_python.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#coding=gbk
+
+print("蔡朝勇开始学习python")
+
+print("hello world")
+
+
---
+++ run_learn_python.bat
@@ -0,0 +1,3 @@
+cd C:\Users\ccy\Desktop\learn_python
+python learn_python.py
+pause
