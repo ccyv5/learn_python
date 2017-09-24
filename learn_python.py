10f0c3643210e53ba04566db2c3de0519175295c
ccyv5 <991522275@qq.com>
2017-09-24 23:15:03 +0800

    第1次提交 ######                  输入与输出                     ###### ######                  注意点                         ###### ######                  数据类型和变量                 ######

--- learn_python.py
+++ learn_python.py
@@ -1,9 +1,122 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#coding=gbk
+# coding=gbk                    使用中文注释必须添加
 
 print("蔡朝勇开始学习python")
-
 print("hello world")
+
+
+######                  输入与输出                     ######
+#输出
+print("输入与输出")
+#输出多个字符串
+print('123','456','789')
+#逗号会变成 空格出书
+
+#输出整数
+print(100+200)
+
+#输入
+#name = input()                     #该函数获取的是一个字符串
+#name = input('please enter your name: ')   #该函数提供一个字符串提醒
+#print("name=",name)
+######                  输入与输出                     ######
+
+######
+
+
+######                  注意点                         ######
+#'#'注释
+#':'语句结束
+#'缩进'语句块
+#tab应该这是4个空格的缩进
+#python 区分大小写
+######                  注意点                         ######
+
+
+######                  数据类型和变量                 ######
+
+
+
+######                  数据类型和变量                 ######
+print("数据类型和变量")
+#整数      1 2 3
+#浮点数    0.000123    1.23e4
+#字符串    "abcdefg"
+
+#字符串中的转义字符
+print("ccyv5\'ccyv5")
+
+#不许转义功能       r""
+print(r"ccyv5'ccyv5")
+
+#多行显示   \n
+print("ccyv5\nccyv6")
+
+#多行显示'''...'''
+print('''ccyv7
+ccyv8
+ccyv9''')
+
+#布尔值
+#   True False
+print(True)
+print(False)
+
+#   and or not 运算
+print("True and True")
+print(True and True)
+print("True and False")
+print(True and False)
+print("not   True")
+print(not   True)
+print("False or False")
+print(False or False)
+print("True  or False")
+print(True  or False)
+
+#空值 None    不是0
+print(None)
+
+#变量
+#变量的类型 是随时可以变化的，反复赋值变更新的类型
+#值是在内存中的，变量相当于指针，指向内存
+a = 1
+print("a= ",a)
+a="a_str"
+print("a= ",a)
+a=True
+print("a= ",a)
+a=None
+print("a= ",a)
+
+#常量
+#没有真正意义上的常量
+#以 全部大写命名 常量
+
+#除法 俩种除法
+
+#整数除 
+print(10//3)
+#取余数
+print(10%3)
+#浮点数除
+print(10/3)
+
+######                  数据类型和变量                 ######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
